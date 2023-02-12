8d223d373e2259d1ec5b553c885d3ad95e0abbae
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-12 22:42:54 +0900

    課題提出

---
+++ src/Break01.java
@@ -0,0 +1,18 @@
+
+public class Break01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		for (int count = 0; count < 5; count++) {
+			if(count == 2){
+				break;
+			}
+			
+			System.out.println("Break01 = " + count);
+			
+		}
+
+	}
+
+}
